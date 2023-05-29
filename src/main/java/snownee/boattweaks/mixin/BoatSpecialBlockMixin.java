84611066fa6b50be62b9ae574892c34e501c5c9c
Snownee <i@snownee.com>
2023-05-29 16:28:24 +0800

    multiple tier ejecting

--- src/main/java/snownee/boattweaks/mixin/BoatSpecialBlockMixin.java
+++ src/main/java/snownee/boattweaks/mixin/BoatSpecialBlockMixin.java
@@ -19,7 +19,7 @@ import snownee.boattweaks.duck.BTBoostingBoat;
 @Mixin(Boat.class)
 public abstract class BoatSpecialBlockMixin implements BTBoostingBoat {
 
-	private boolean boattweaks$eject;
+	private int boattweaks$eject;
 	private int boattweaks$boostTicks;
 
 	@Inject(
@@ -27,11 +27,25 @@ public abstract class BoatSpecialBlockMixin implements BTBoostingBoat {
 			value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getBlock()Lnet/minecraft/world/level/block/Block;", ordinal = 1
 	), locals = LocalCapture.CAPTURE_FAILHARD
 	)
-	private void boattweaks$getGroundFriction(CallbackInfoReturnable<Float> cir, AABB aABB, AABB aABB2, int i, int j, int k, int l, int m, int n, VoxelShape voxelShape, float f, int o, BlockPos.MutableBlockPos mutableBlockPos, int p, int q, int r, int s, BlockState blockState) {
+	private void boattweaks$getGroundFriction(CallbackInfoReturnable<Float> cir, AABB aABB, AABB aABB2, int i, int j, int k, int l, int m, int n, VoxelShape voxelShape, float f, int o, BlockPos.MutableBlockPos pos, int p, int q, int r, int s, BlockState blockState) {
 		Boat boat = (Boat) (Object) this;
-		if (!boattweaks$eject && blockState.is(BoatTweaks.CONFIG.ejectingBlock)) {
-			boattweaks$eject = true;
-			boat.playSound(BoatTweaks.EJECT.get());
+		if (blockState.is(BoatTweaks.CONFIG.ejectingBlock)) {
+			if (boattweaks$eject == 0) {
+				boat.playSound(BoatTweaks.EJECT.get());
+			}
+			int eject = 1;
+			int y = pos.getY();
+			BlockState blockState1;
+			while (true) {
+				pos.setY(y - eject);
+				blockState1 = boat.level.getBlockState(pos);
+				if (!blockState1.is(BoatTweaks.CONFIG.ejectingBlock)) {
+					break;
+				}
+				eject++;
+			}
+			boattweaks$eject = Math.max(boattweaks$eject, eject);
+			pos.setY(y);
 		}
 		if (boattweaks$boostTicks < BoatTweaks.CONFIG.boostingTicks && blockState.is(BoatTweaks.CONFIG.boostingBlock)) {
 			if (BoatTweaks.CONFIG.boostingTicks - boattweaks$boostTicks > 10) {
@@ -44,9 +58,15 @@ public abstract class BoatSpecialBlockMixin implements BTBoostingBoat {
 	@Inject(method = "tick", at = @At("HEAD"))
 	private void boattweaks$tick(CallbackInfo ci) {
 		Boat boat = (Boat) (Object) this;
-		if (boattweaks$eject) {
-			boattweaks$eject = false;
-			boat.setDeltaMovement(boat.getDeltaMovement().with(Direction.Axis.Y, BoatTweaks.CONFIG.ejectingForce));
+		if (boattweaks$eject > 0) {
+			float force;
+			if (boattweaks$eject == 1) {
+				force = BoatTweaks.CONFIG.ejectingForce;
+			} else {
+				force = BoatTweaks.CONFIG.ejectingForce * (float) (Math.pow(1.1, boattweaks$eject - 1));
+			}
+			boattweaks$eject = 0;
+			boat.setDeltaMovement(boat.getDeltaMovement().with(Direction.Axis.Y, force));
 		}
 		if (boattweaks$boostTicks > 0) {
 			boattweaks$boostTicks--;
